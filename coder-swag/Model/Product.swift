bcb8d09faa7723829b012c9fd3e11bfca53d1878
Sascha Landegge <saschalandegge@live.nl>
2018-01-05 18:16:40 +0100

    finished lesson 3

---
+++ coder-swag/Model/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Sascha Landegge on 05/01/2018.
+//  Copyright © 2018 Codeunicum. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        <#statements#>
+    }
+}
---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Sascha Landegge on 05/01/2018.
+//  Copyright © 2018 Codeunicum. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
